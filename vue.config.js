a47cf80382830e53804b9162ac57eca54ffe29b3
Gerard Sans <gsans@amazon.com>
2020-09-13 21:44:49 +0100

    Tidy up

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,6 @@
-// const { InjectManifest } = require('workbox-webpack-plugin')
-
-// //optional, but InjectManifest didn't respect the {mode: 'development'} config
-// process.env.NODE_ENV = 'development'
-
 const manifest = require('./public/manifest.json')
 
 module.exports = {
-  // configureWebpack: {
-  //   plugins: [
-  //     new InjectManifest({
-  //       swSrc: './src/registerServiceWorker.js',
-  //       swDest: 'service-worker.js'
-  //     })
-  //   ]
-  // },
   pwa: {
     name: manifest.short_name,
     themeColor: manifest.theme_color,
